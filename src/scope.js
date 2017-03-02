cdb3e4cbc9dee3cb4207a32c2e004c8748a3eac3
James Squillante <jjsquillante@gmail.com>
2017-03-01 19:29:35 -0600

    1.12 Destroying a watch

--- src/scope.js
+++ src/scope.js
@@ -18,32 +18,43 @@ function Scope() {
 function initWatchVal() {}
 
 Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
+	var self = this;
 	var watcher = {
 		watchFn: watchFn,
 		listenerFn: listenerFn || function() {},
 		valueEq: !!valueEq,
 		last: initWatchVal
 	};
-	this.$$watchers.push(watcher);
+	this.$$watchers.unshift(watcher);
 	this.$$lastDirtyWatch = null;
+
+	return function() {
+		var index = self.$$watchers.indexOf(watcher);
+		if (index >= 0) {
+			self.$$watchers.splice(index, 1);
+			self.$$lastDirtyWatch = null; // set to null on removal so lastDirtyWatch does not cause shortCircuit
+		}
+	};
 };
 
 Scope.prototype.$$digestOnce = function() {
 	var self = this;
 	var newValue, oldValue, dirty;
-	_.forEach(this.$$watchers, function(watcher) {
+	_.forEachRight(this.$$watchers, function(watcher) {
 		try {
-			newValue = watcher.watchFn(self);
-			oldValue = watcher.last;
-			// if !(false) ==> true, invoke listenerFn, etc.
-			if(!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
-				self.$$lastDirtyWatch = watcher;
-				watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
-				watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
-				dirty = true;
-			} else if (self.$$lastDirtyWatch === watcher) {
-				return false;
-			}				
+			if(watcher) {
+				newValue = watcher.watchFn(self);
+				oldValue = watcher.last;
+				// if !(false) ==> true, invoke listenerFn, etc.
+				if(!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+					self.$$lastDirtyWatch = watcher;
+					watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+					watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
+					dirty = true;
+				} else if (self.$$lastDirtyWatch === watcher) {
+					return false;
+				}
+			}
 		} catch (e) {
 			console.error(e);
 		}
