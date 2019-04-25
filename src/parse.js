485ad378db7cae745cf08655faf1e59185c449b0
James Squillante <jjsquillante@gmail.com>
2019-04-24 21:18:28 -0500

    Chapter 6 - Parsing Integers && Parsing Floating Point Numbers.

--- src/parse.js
+++ src/parse.js
@@ -6,7 +6,39 @@ function Lexer() {}
 
 // Executes tokenization.
 Lexer.prototype.lex = function (text) {
+	this.text = text;
+	this.index = 0;
+	this.ch = undefined;
+	this.tokens = [];
 
+	while (this.index < this.text.length) {
+		this.ch = this.text.charAt(this.index);
+		if (this.isNumber(this.ch)) {
+			this.readNumber();
+		} else {
+			throw 'Unexpected next character: ' + this.ch;
+		}
+	}
+	return this.tokens;
+};
+Lexer.prototype.isNumber = function (ch) {
+	return '0' <= ch && ch <= '9';
+};
+Lexer.prototype.readNumber = function () {
+	var number = '';
+	while (this.index < this.text.length) {
+		var ch = this.text.charAt(this.index);
+		if (ch === '.' || this.isNumber(ch)) {
+			number += ch;
+		} else {
+			break;
+		}
+		this.index++;
+	}
+	this.tokens.push({
+		text: number,
+		value: Number(number)
+	});
 };
 
 function AST(lexer) {
@@ -16,7 +48,16 @@ function AST(lexer) {
 // AST building will be done here.
 AST.prototype.ast = function (text) {
 	this.tokens = this.lexer.lex(text);
+	return this.program();
 };
+AST.prototype.program = function () {
+	return { type: AST.Program, body: this.constant() };
+};
+AST.prototype.constant = function () {
+	return { type: AST.Literal, value: this.tokens[0].value };
+};
+AST.Program = 'Program';
+AST.Literal = 'Literal';
 
 function ASTCompiler (astBuilder) {
 	this.astBuilder = astBuilder;
@@ -25,6 +66,20 @@ function ASTCompiler (astBuilder) {
 // AST compilation will be done here.
 ASTCompiler.prototype.compile = function (text) {
 	var ast = this.astBuilder.ast(text);
+	this.state = { body: [] };
+	this.recurse(ast);
+	 /* jshint -W054 */
+	 return new Function(this.state.body.join(''));
+	 /* jshint +W054 */
+};
+ASTCompiler.prototype.recurse = function (ast) {
+	switch (ast.type) {
+		case AST.Program:
+			this.state.body.push('return ', this.recurse(ast.body), ';');
+			break;
+		case AST.Literal:
+			return ast.value
+	}
 };
 
 function Parser(lexer) {
---
+++ test/parse_spec.js
@@ -0,0 +1,16 @@
+'use-strict';
+
+var parse = require('../src/parse');
+
+describe('parse', function () {
+		it('can parse an integer', function () {
+			var fn = parse('42');
+			expect(fn).toBeDefined();
+			expect(fn()).toBe(42);
+		});
+		it('can parse a floating point number', function () {
+			var fn = parse('4.2');
+			console.log(fn.toString());
+			expect(fn()).toBe(4.2);
+		});
+});
