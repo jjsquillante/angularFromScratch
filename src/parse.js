4ef6cdc67cb56928af0e67b99afce927ea551f9e
James Squillante <jjsquillante@gmail.com>
2019-05-02 21:23:17 -0500

    Chapter 6 - parsing true, false, and null.

--- src/parse.js
+++ src/parse.js
@@ -27,15 +27,19 @@ Lexer.prototype.lex = function (text) {
 			this.readNumber();
 		} else if (this.ch === '\'' || this.ch === '"') {
 			this.readString(this.ch);
+		} else if (this.isIdent(this.ch)) {
+			this.readIdent();
 		} else {
 			throw 'Unexpected next character: ' + this.ch;
 		}
 	}
 	return this.tokens;
 };
+
 Lexer.prototype.isNumber = function (ch) {
 	return '0' <= ch && ch <= '9';
 };
+
 Lexer.prototype.readNumber = function () {
 	var number = '';
 	while (this.index < this.text.length) {
@@ -62,14 +66,17 @@ Lexer.prototype.readNumber = function () {
 		value: Number(number)
 	});
 };
+
 Lexer.prototype.peek = function () {
 	return this.index < this.text.length - 1 ?
 		this.text.charAt(this.index + 1) :
 		false;
 };
+
 Lexer.prototype.isExpOperator = function (ch) {
 	return ch === '-' || ch === '+' || this.isNumber(ch);
 };
+
 Lexer.prototype.readString = function (quote) {
 	// skip forward past the \' or \" denoting the beginning of a string.
 	this.index++;
@@ -111,23 +118,60 @@ Lexer.prototype.readString = function (quote) {
 	throw 'Unmatched quote';
 };
 
+Lexer.prototype.isIdent = function (ch) {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ||
+		ch === '_' || ch === '$';
+};
+
+Lexer.prototype.readIdent = function () {
+	var text = '';
+	while (this.index < this.text.length) {
+		var ch = this.text.charAt(this.index);
+		if (this.isIdent(ch) || this.isNumber(ch)) {
+			text += ch;
+		} else {
+			break;
+		}
+		this.index++;
+	}
+	var token = { text: text };
+	this.tokens.push(token);
+};
+
 function AST(lexer) {
 	this.lexer = lexer;
 }
 
 // AST building will be done here.
+
 AST.prototype.ast = function (text) {
 	this.tokens = this.lexer.lex(text);
 	return this.program();
 };
+
 AST.prototype.program = function () {
-	return { type: AST.Program, body: this.constant() };
+	return { type: AST.Program, body: this.primary() };
 };
+
+AST.prototype.primary = function () {
+	if (this.constants.hasOwnProperty(this.tokens[0].text)) {
+		return this.constants[this.tokens[0].text];
+	} else {
+		return this.constant();
+	}
+};
+
 AST.prototype.constant = function () {
 	return { type: AST.Literal, value: this.tokens[0].value };
 };
+
 AST.Program = 'Program';
 AST.Literal = 'Literal';
+AST.prototype.constants = {
+	'null': { type: AST.Literal, value: null },
+	'true': { type: AST.Literal, value: true },
+	'false': { type: AST.Literal, value: false }
+};
 
 function ASTCompiler (astBuilder) {
 	this.astBuilder = astBuilder;
@@ -142,6 +186,7 @@ ASTCompiler.prototype.compile = function (text) {
 	 return new Function(this.state.body.join(''));
 	 /* jshint +W054 */
 };
+
 ASTCompiler.prototype.recurse = function (ast) {
 	switch (ast.type) {
 		case AST.Program:
@@ -155,11 +200,15 @@ ASTCompiler.prototype.recurse = function (ast) {
 ASTCompiler.prototype.escape = function (value) {
 	if (_.isString(value)) {
 		return '\'' + value.replace(this.stringEscapeRegex, this.stringEscapeFn) + '\'';
+	} else if (_.isNull(value)) {
+		return 'null';
 	} else {
 		return value;
 	}
 };
+
 ASTCompiler.prototype.stringEscapeRegex = /[^ a-z0-9]/gi;
+
 ASTCompiler.prototype.stringEscapeFn = function (c) {
 	return '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4);
 };
