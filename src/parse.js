82246707635ca18eeba190af73230a935122209d
James Squillante <jjsquillante@gmail.com>
2019-05-22 21:06:33 -0500

    7.2 - Parsing this.

--- src/parse.js
+++ src/parse.js
@@ -169,11 +169,13 @@ AST.ArrayExpression = 'ArrayExpression';
 AST.ObjectExpression = 'ObjectExpression';
 AST.Property = 'Property';
 AST.Identifier = 'Identifier';
+AST.ThisExpression = 'ThisExpression';
 
 AST.prototype.constants = {
 	'null': { type: AST.Literal, value: null },
 	'true': { type: AST.Literal, value: true },
-	'false': { type: AST.Literal, value: false }
+	'false': { type: AST.Literal, value: false },
+	'this': { type: AST.ThisExpression }
 };
 
 AST.prototype.ast = function (text) {
@@ -307,6 +309,8 @@ ASTCompiler.prototype.recurse = function (ast) {
 			var intoId = this.nextId();
 			this.if_('s', this.assign(intoId, this.nonComputedMember('s', ast.name)));
 			return intoId;
+		case AST.ThisExpression:
+			return 's';
 	}
 };
 
--- test/parse_spec.js
+++ test/parse_spec.js
@@ -120,26 +120,38 @@ describe('parse', function () {
 			var fn = parse('[1, 2, 3, ]');
 			expect(fn()).toEqual([1,2,3]);
 		});
+
 		it('will parse an empty object.', function () {
 			var fn = parse('{}');
 			expect(fn()).toEqual({});
 		});
+
 		it('will parse a non-empty object.', function () {
 			var fn = parse('{"a key": 1, \'another-key\':2}');
 			expect(fn()).toEqual({'a key': 1, 'another-key': 2 });
 		});
+
 		it('will parse an object with identifier keys.', function () {
 			var fn = parse('{a: 1, b: [2, 3], c: { d: 4}}');
 			expect(fn()).toEqual({ a: 1, b: [2, 3], c: {d: 4 }});
 		});
+
 		// Chapter 7
 		it('looks up an attribute from the scope.', function () {
 			var fn = parse('aKey');
 			expect(fn({ aKey: 42 })).toBe(42);
 			expect(fn({})).toBeUndefined();
 		});
+
 		it('returns undefined when looking up attribute from undefined.', function () {
 			var fn = parse('aKey');
+			expect(fn()).toBeUndefined();
+		});
+
+		it('will parse this.', function () {
+			var fn = parse('this');
+			var scope = {};
+			expect(fn(scope)).toBe(scope);
 			expect(fn()).toBeUndefined();
 		});
 });
