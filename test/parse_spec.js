2d5b915217c050dcb3d589420160e4ef9dd80a4c
James Squillante <jjsquillante@gmail.com>
2019-06-20 20:51:53 -0500

    7.5 - Computed Attribute Lookup.

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -208,4 +208,25 @@ describe('parse', function () {
 			locals = { aKey: 43 };
 			expect(fn(scope, locals)).toBe(43);
 		});
+
+		// computed attribute lookup
+		it('parses a simple computed property access.', function () {
+			var fn = parse('aKey["anotherKey"]');
+			expect(fn({ aKey: { anotherKey: 42}})).toBe(42);
+		});
+
+		it('parses a computed numeric array access.', function () {
+			var fn = parse('anArray[1]');
+			expect(fn({ anArray: [1,2,3]})).toBe(2);
+		});
+
+		it('parses a computed access with another key as property.', function () {
+			var fn = parse('lock[key]');
+			expect(fn({ key: 'theKey', lock: { theKey: 42}})).toBe(42);
+		});
+
+		it('parses computed access with another access as property.', function () {
+			var fn = parse('lock[keys["aKey"]]');
+			expect(fn({keys: { aKey: 'theKey'}, lock: { theKey: 42}})).toBe(42);
+		});
 });
