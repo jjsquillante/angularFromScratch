ae1d2cce257edd46dd138de098c0ee86a3d787bd
James Squillante <jjsquillante@gmail.com>
2019-10-31 10:50:28 -0500

    8.8 Statements

--- src/parse.js
+++ src/parse.js
@@ -90,7 +90,7 @@ Lexer.prototype.lex = function (text) {
 		} else if (this.is('\'"')) {
 			this.readString(this.ch);
 			// reserved character tokens to look for.
-		} else if (this.is('[],{}:.()?')) {
+		} else if (this.is('[],{}:.()?;')) {
 			this.tokens.push({
 				text: this.ch
 			});
@@ -271,7 +271,15 @@ AST.prototype.ast = function (text) {
 };
 
 AST.prototype.program = function () {
-	return { type: AST.Program, body: this.assignment() };
+	var body = [];
+	while (true) {
+		if (this.tokens.length) {
+			body.push(this.assignment());
+		}
+		if (!this.expect(';')) {
+			return { type: AST.Program, body: body };
+		}
+	}
 };
 
 AST.prototype.primary = function () {
@@ -563,7 +571,10 @@ ASTCompiler.prototype.recurse = function (ast, context, create) {
 	var intoId;
 	switch (ast.type) {
 		case AST.Program:
-			this.state.body.push('return ', this.recurse(ast.body), ';');
+			_.forEach(_.initial(ast.body), _.bind(function(stmt) {
+				this.state.body.push(this.recurse(stmt), ';');
+			}, this));
+			this.state.body.push('return ', this.recurse(_.last(ast.body)), ';');
 			break;
 		case AST.Literal:
 			return this.escape(ast.value);
